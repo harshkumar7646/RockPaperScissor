63d5431e0685b92fd70b4c01213ef1517d29d6a1
harshkumar7646 <sirihey0047@gmail.com>
2024-10-17 20:33:40 +0530

    added stonePaper.js

---
+++ stonePaper.js
@@ -0,0 +1,92 @@
+let rock=document.querySelector(".stone");
+let paper=document.querySelector(".paper");
+let scissor=document.querySelector(".scissor");
+let para=document.querySelector("p");
+let score1=document.querySelector("#computer-score");
+let score2=document.querySelector("#us");
+let paradiv=document.querySelector(".txt");
+let btn1=document.querySelector(".btn1");
+let btn2=document.querySelector(".btn2");
+let heading=document.querySelector("#heading");
+let x=0;
+let comp=0;
+function scorerecorder1(){
+    let ran=Math.floor(Math.random()*3);
+    if(ran==0){
+        para.innerText="computer chosen rock";
+        paradiv.style.backgroundColor="red";
+    }
+    else if(ran==1){
+        para.innerText="computer chosen paper";
+        paradiv.style.backgroundColor="green";
+        comp=comp+1;
+        score1.innerText=comp;
+    }
+    else if(ran==2){
+        x=x+1;
+        console.log(x)
+        score2.innerText=x;
+        paradiv.style.backgroundColor="green";
+        para.innerText="computer chosen scissor";
+    }
+}
+function scorerecorder2(){
+    let ran=Math.floor(Math.random()*3);
+    if(ran==0){
+        para.innerText="computer chosen rock";
+        x=x+1;
+        score2.innerText=x;
+        paradiv.style.backgroundColor="green";
+    }
+    else if(ran==1){
+        para.innerText="computer chosen paper";
+        paradiv.style.backgroundColor="red";
+    }
+    else if(ran==2){
+        para.innerText="computer chosen scissor";
+        comp=comp+1;
+        score1.innerText=comp;
+        paradiv.style.backgroundColor="green";
+
+    }
+}
+function scorerecorder3(){
+    let ran=Math.floor(Math.random()*3);
+    if(ran==0){
+        para.innerText="computer chosen rock";
+        comp=comp+1;
+        score1.innerText=comp;
+        paradiv.style.backgroundColor="green";
+    }
+    else if(ran==1){
+        para.innerText="computer chosen paper";
+        x=x+1;
+        score2.innerText=x;
+        paradiv.style.backgroundColor="green";
+    }
+    else if(ran==2){
+        para.innerText="computer chosen scissor";
+        paradiv.style.backgroundColor="red";
+    }
+}
+rock.addEventListener("click",()=>{
+    scorerecorder1();
+})
+paper.addEventListener("click",()=>{
+    scorerecorder2();
+})
+scissor.addEventListener("click",()=>{
+    scorerecorder3();
+})
+btn1.addEventListener("click",()=>{
+    document.querySelector(".main").style.backgroundColor="white";
+    score1.style.color="black";
+    score2.style.color="black";
+    heading.style.color="black";
+})
+btn2.addEventListener("click",()=>{
+    document.querySelector(".main").style.backgroundColor="black";
+    score1.style.color="white";
+    score2.style.color="white";
+    heading.style.color="white";
+})
